f17f1f00b1be497a5cc7fc1b1b8f3e8f2602f2a1
Yuichi Hezemans <61275852+Yuichi135@users.noreply.github.com>
2023-01-20 20:50:03 +0100

    initial commit

---
+++ src/Cell.java
@@ -0,0 +1,44 @@
+import javafx.scene.shape.Rectangle;
+
+public class Cell {
+    private int x;
+    private int y;
+    private boolean isAlive;
+    private boolean nextLife;
+    private Rectangle square;
+
+    public Cell(int x, int y, boolean isAlive, Rectangle square) {
+        this.x = x;
+        this.y = y;
+        this.isAlive = isAlive;
+        this.nextLife = false;
+        this.square = square;
+
+        this.square.setFill(GameOfLife.getColor(this.isAlive));
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public boolean isAlive() {
+        return this.isAlive;
+    }
+
+    public void kill() {
+        this.nextLife = false;
+    }
+
+    public void revive() {
+        this.nextLife = true;
+    }
+
+    public void update() {
+        this.isAlive = this.nextLife;
+        this.square.setFill(GameOfLife.getColor(this.isAlive));
+    }
+}
---
+++ src/GameOfLife.java
@@ -0,0 +1,97 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class GameOfLife extends Application {
+    public static final byte CELLSIZE = 4;
+    public static final int GRIDWIDTH = 300;
+    public static final int GRIDHEIGHT = 200;
+    public static final Color ALIVE_COLOR = Color.WHITE;
+    public static final Color DEAD_COLOR = Color.BLACK;
+    private final ArrayList<ArrayList<Cell>> cells = new ArrayList<>();
+
+    @Override
+    public void start(Stage stage) {
+        GridPane gridPane = new GridPane();
+
+        for (int x = 0; x < GRIDWIDTH; x++) {
+            cells.add(new ArrayList<>());
+
+            for (int y = 0; y < GRIDHEIGHT; y++) {
+                Rectangle square = new Rectangle(CELLSIZE, CELLSIZE);
+                cells.get(x).add(new Cell(x, y, this.randomBool(), square));
+
+                gridPane.add(square, x, y);
+            }
+        }
+
+        Scene scene = new Scene(gridPane);
+
+        stage.setScene(scene);
+        stage.setTitle("Conway's Game of Life");
+        stage.show();
+
+        scene.setOnScroll(event -> this.update());
+    }
+
+    private boolean randomBool() {
+        return Math.random() > .5;
+    }
+
+    public static Color getColor(boolean bool) {
+        return (bool) ? ALIVE_COLOR : DEAD_COLOR;
+    }
+
+    private static Color randomColor() {
+        return Color.rgb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
+    }
+
+    private boolean isAliveNextUpdate(Cell cell) {
+        boolean isCurrentlyAlive = cell.isAlive();
+
+        int x = cell.getX();
+        int y = cell.getY();
+        byte neighbours = 0;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                try {
+                    if (this.cells.get(i + x).get(j + y).isAlive())
+                        neighbours++;
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        if (isCurrentlyAlive)
+            neighbours--;
+
+        if (isCurrentlyAlive) {
+            return neighbours == 2 || neighbours == 3;
+        } else {
+            return neighbours == 3;
+        }
+    }
+
+    public void update() {
+        for (ArrayList<Cell> cellList : cells) {
+            for (Cell cell : cellList) {
+                if (this.isAliveNextUpdate(cell)) {
+                    cell.revive();
+                } else {
+                    cell.kill();
+                }
+            }
+        }
+
+        for (ArrayList<Cell> cellList : cells) {
+            for (Cell cell : cellList) {
+                cell.update();
+            }
+        }
+    }
+}
