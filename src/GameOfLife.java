a643b667896551ca3a3712e5d56fee08b5e9e732
Yuichi Hezemans <61275852+Yuichi135@users.noreply.github.com>
2023-02-07 21:53:14 +0100

    Total revamp, way more optimized

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -2,49 +2,45 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.util.ArrayList;
-
 public class GameOfLife extends Application {
-    public static final byte CELLSIZE = 8;
-    public static final int GRIDWIDTH = 200;
-    public static final int GRIDHEIGHT = 100;
-    public static final Color ALIVE_COLOR = Color.WHITE;
-    public static final Color DEAD_COLOR = Color.BLACK;
-    private final ArrayList<ArrayList<Cell>> cells = new ArrayList<>();
+    private final int GRID_WIDTH = 800;
+    private final int GRID_HEIGHT = 400;
+    private final int TILE_SIZE = 2;
+    private boolean[][] currentGrid;
+    private boolean[][] nextGrid;
+    private GraphicsContext graphicsContext;
 
     @Override
-    public void start(Stage stage) {
-        GridPane gridPane = new GridPane();
-        for (int x = 0; x < GRIDWIDTH; x++) {
-            cells.add(new ArrayList<>());
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, GRID_WIDTH * TILE_SIZE, GRID_HEIGHT * TILE_SIZE, Color.BLACK);
+        Canvas canvas = new Canvas(GRID_WIDTH * TILE_SIZE, GRID_HEIGHT * TILE_SIZE);
+        this.graphicsContext = canvas.getGraphicsContext2D();
+        this.graphicsContext.setFill(Color.WHITE);
 
-            for (int y = 0; y < GRIDHEIGHT; y++) {
-                Rectangle square = new Rectangle(CELLSIZE, CELLSIZE);
-                cells.get(x).add(new Cell(x, y, false, square));
-
-                gridPane.add(square, x, y);
-            }
-        }
-
-        gridPane.setOnDragDetected(event -> gridPane.startFullDrag());
-
-        Scene scene = new Scene(gridPane);
-
+        root.getChildren().add(canvas);
         stage.setScene(scene);
         stage.setTitle("Conway's Game of Life");
+        stage.setResizable(false);
         stage.show();
 
-        Timeline continuousUpdate = new Timeline(new KeyFrame(Duration.millis(100), (ActionEvent event) -> this.update()));
-        continuousUpdate.setCycleCount(Timeline.INDEFINITE);
+        this.initGrid();
+        this.draw();
+        Timeline continuousUpdate = new Timeline(new KeyFrame(Duration.millis(25), event -> {
+            this.update();
+            this.draw();
+        }));
+
+        continuousUpdate.setCycleCount(Animation.INDEFINITE);
         continuousUpdate.play();
 
         scene.setOnKeyReleased(event -> {
@@ -56,59 +52,80 @@ public class GameOfLife extends Application {
         });
     }
 
+    private void initGrid() {
+        this.currentGrid = this.createEmptyGrid();
+
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            for (int y = 0; y < GRID_HEIGHT; y++) {
+                this.currentGrid[x][y] = this.randomBool();
+            }
+        }
+    }
+
+    private boolean[][] createEmptyGrid() {
+        boolean[][] grid = new boolean[GRID_WIDTH][];
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            grid[x] = new boolean[GRID_HEIGHT];
+            for (int y = 0; y < GRID_HEIGHT; y++) {
+                grid[x][y] = false;
+            }
+        }
+
+        return grid;
+    }
+
     private boolean randomBool() {
-        return Math.random() > .5;
+        return Math.random() > 0.5;
     }
-
-    public static Color getColor(boolean bool) {
-        return (bool) ? ALIVE_COLOR : DEAD_COLOR;
-    }
-
-    private static Color randomColor() {
-        return Color.rgb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
-    }
-
-    private boolean isAliveNextUpdate(Cell cell) {
-        boolean isCurrentlyAlive = cell.isAlive();
-
-        int x = cell.getX();
-        int y = cell.getY();
-        byte neighbours = 0;
-
+    private int getNeighbours(int x, int y) {
+        int neighbours = 0;
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                try {
-                    if (this.cells.get(i + x).get(j + y).isAlive())
-                        neighbours++;
-                } catch (Exception ignored) {
-                }
+                if (this.currentGrid[(i + x + GRID_WIDTH) % GRID_WIDTH][(j + y + GRID_HEIGHT) % GRID_HEIGHT])
+                    neighbours++;
             }
         }
-        if (isCurrentlyAlive)
+
+        if (this.currentGrid[x][y])
             neighbours--;
 
-        if (isCurrentlyAlive) {
-            return neighbours == 2 || neighbours == 3;
-        } else {
-            return neighbours == 3;
-        }
+        return neighbours;
     }
 
-    public void update() {
-        for (ArrayList<Cell> cellList : cells) {
-            for (Cell cell : cellList) {
-                if (this.isAliveNextUpdate(cell)) {
-                    cell.revive();
+    private void clearCanvas() {
+        this.graphicsContext.clearRect(0, 0, GRID_WIDTH * TILE_SIZE, GRID_HEIGHT * TILE_SIZE);
+    }
+
+
+    private void update() {
+        this.nextGrid = this.createEmptyGrid();
+
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            for (int y = 0; y < GRID_HEIGHT; y++) {
+                if (this.currentGrid[x][y]) {
+                    this.nextGrid[x][y] = this.getNeighbours(x, y) == 2 || this.getNeighbours(x, y) == 3;
                 } else {
-                    cell.kill();
+                    this.nextGrid[x][y] = this.getNeighbours(x, y) == 3;
                 }
             }
         }
 
-        for (ArrayList<Cell> cellList : cells) {
-            for (Cell cell : cellList) {
-                cell.update();
+        System.arraycopy(this.nextGrid, 0, this.currentGrid, 0, this.currentGrid.length);
+    }
+
+    private void draw() {
+        this.clearCanvas();
+
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < GRID_WIDTH; i++) {
+            for (int j = 0; j < GRID_HEIGHT; j++) {
+                if (this.currentGrid[i][j])
+                    this.graphicsContext.fillRect(x, y, TILE_SIZE, TILE_SIZE);
+                y += TILE_SIZE;
             }
+            x += TILE_SIZE;
+            y = 0;
         }
     }
 }
